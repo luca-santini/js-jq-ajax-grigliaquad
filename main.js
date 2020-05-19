7725472904c666c3e4321cd42dbf89665f835eda
Luca Santini <lucasantinidev@gmail.com>
2020-05-19 17:41:27 +0200

    ajax

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-// Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
-// Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
-// In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
+// Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9. Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde. In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
 
 $(document).ready(function(){
+    // genero un numero casuale compreso tra 1 e 9 con la funzione getRandomIntInclusive
+    function getRandomIntInclusive (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;} //Il max è incluso e il min è incluso
 
-  $(".quadrato").click(function(){
-      var numero =  prompt(parseInt('inserisci un numero'));
-      console.log(numero);
+  // quando clicco sul quadrato parte una richiesta ajax per recuperare un numero random tra 1 e 9.
+  var numero_random = (getRandomIntInclusive(1, 9));
+  $(this).text(numero_random);
+
+  // faccio una chiamata ajax per recuperare il numero casuale
+  $.ajax({
+      'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+      'method': 'GET',
+      'success': function(data) {
+          // recupero il numero restituito dall'api
+          var numero_random = data.response;
+          console.log('Numero random: ' + numero_random);
+      },
+      'error': function() {
+          alert('si è verificato un errore');
+      }
   });
+
 });
+// Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
+function colore_quadrato(numero_random) {
+    if(numero_random < 5) {
+        // il quadrato su cui l'utente ha cliccato diventa giallo;
+        $(this).addClass('yellow');
+    } else {
+        // il quadrato su cui l'utente ha cliccato diventa verde.
+        $(this).addClass('green');
+    }
+}
