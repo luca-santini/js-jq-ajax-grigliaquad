d18787e17025d249423598b5b50e24e40ad4143a
Luca Santini <lucasantinidev@gmail.com>
2020-05-20 10:16:48 +0200

    utilizzo jquery per evitare di ripetere 36 volte div nell'html

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 // Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9. Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde. In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
 
 $(document).ready(function(){
-  //   // genero un numero casuale compreso tra 1 e 9 con la funzione getRandomIntInclusive
-  //   function getRandomIntInclusive (min, max) {
-  //     min = Math.ceil(min);
-  //     max = Math.floor(max);
-  //     return Math.floor(Math.random() * (max - min + 1)) + min;} //Il max è incluso e il min è incluso
-  //
-  // // quando clicco sul quadrato parte una richiesta ajax per recuperare un numero random tra 1 e 9.
-  // var numero_random = (getRandomIntInclusive(1, 9));
-  // $(this).text(numero_random);
+ // utilizzo jquery per evitare di ripetere 36 volte la classe quadrato nel html
+ for (var i = 0; i < 36; i++) {
+     $('#container').append('<div class="quadrato"></div>');
+ }
 
   // faccio una chiamata ajax per recuperare il numero casuale
   $('.quadrato').click(function() {
